cda59e7338844e4b1de1af82c706bffa91cce987
chenwencheng <chenwencheng@4399inc.com>
2022-05-06 17:29:15 +0800

    解决问题了

--- src/lib/EventButton.svelte
+++ src/lib/EventButton.svelte
@@ -4,16 +4,27 @@
 
 <script lang="ts">
   export let type: Type = "normal";
+
+  const textColorMapper: Record<Type, string> = {
+    important: "text-red-400",
+    cancel: "text-grat-400",
+    confirm: "text-yellow-400",
+    normal: "text-blue-400",
+  };
+
+  const borderColorMapper: Record<Type, string> = {
+    important: "border-red-400",
+    cancel: "border-grat-400",
+    confirm: "border-yellow-400",
+    normal: "border-blue-400",
+  };
+
+  $: textColor = textColorMapper[type] ?? "text-blue-400";
+  $: borderColor = borderColorMapper[type] ?? "border-blue-400";
 </script>
 
 <button
-  class="h-[34px] w-[60px] border-solid border-[1px] select-none text-blue-400 border-blue-400"
-  class:text-red-400={type === "important"}
-  class:border-red-400={type === "important"}
-  class:text-gray-400={type === "cancel"}
-  class:border-gray-400={type === "cancel"}
-  class:text-yellow-400={type === "confirm"}
-  class:border-yellow-400={type === "confirm"}
+  class="h-[34px] w-[60px] border-solid border-[1px] select-none {textColor} {borderColor}"
   on:click
 >
   <slot />
