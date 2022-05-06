6d27c8bd2dec1ca88cd5fc2c7b0f8edf9d71fbb1
chenwencheng <chenwencheng@4399inc.com>
2022-05-06 15:08:24 +0800

    切换到windi

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import App from './App.svelte'
+import "virtual:windi.css"
 
-import "./main.css"
 const app = new App({
   target: document.getElementById('app')
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import WindiCSS from "vite-plugin-windicss"
 import { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), WindiCSS()],
   resolve: {
     alias: {
       "@": resolve("src")
