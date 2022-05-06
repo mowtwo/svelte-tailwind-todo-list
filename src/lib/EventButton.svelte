54114352635a61e99d305bc7950804ee0d5d14d4
chenwencheng <chenwencheng@4399inc.com>
2022-05-06 14:47:05 +0800

    尝试解决生成样式问题

--- src/lib/EventButton.svelte
+++ src/lib/EventButton.svelte
@@ -18,6 +18,12 @@
   };
 </script>
 
+<!-- preload tailwind -->
+<div class="hidden text-red-400 border-red-400" />
+<div class="hidden text-gray-400 border-gray-400" />
+<div class="hidden text-yellow-400 border-yellow-400" />
+<div class="hidden text-blue-400 border-blue-400" />
+
 <button
   class="text-{getColorType(type)}-400 h-[34px] w-[60px] border-{getColorType(
     type
